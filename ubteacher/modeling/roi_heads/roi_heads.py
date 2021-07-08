162f8d3d3bd7d5b90bc21ab44a2c0a298a392c8c
Eleonora <eleonora.iotti@externals.adidas.com>
2021-07-08 17:26:21 +0200

    Non-Local Block v1.0 whithout-Convolution

--- ubteacher/modeling/roi_heads/roi_heads.py
+++ ubteacher/modeling/roi_heads/roi_heads.py
@@ -5,6 +5,12 @@ from detectron2.structures import Boxes, ImageList, Instances, pairwise_iou
 from detectron2.modeling.proposal_generator.proposal_utils import (
     add_ground_truth_to_proposals,
 )
+
+from torch import nn #
+from torch.nn import functional as F #
+from .conv_module import ConvModule #
+
+
 from detectron2.utils.events import get_event_storage
 from detectron2.modeling.roi_heads.box_head import build_box_head
 from detectron2.layers import ShapeSpec
@@ -18,6 +24,9 @@ from ubteacher.modeling.roi_heads.fast_rcnn import FastRCNNFocaltLossOutputLayer
 import numpy as np
 from detectron2.modeling.poolers import ROIPooler, convert_boxes_to_pooler_format
 
+
+
+
 def build_pre_processing(cfg):
     
     conv_type=cfg.MODEL.ROI_CONV_PRE
@@ -30,7 +39,9 @@ def build_pre_processing(cfg):
     
     elif conv_type == "Conv7":
         pre_module=torch.nn.Conv2d(256,256,kernel_size=7,padding=3)
-
+        
+    elif conv_type == "NL":
+        post_module=NonLocal2d(256)
     return pre_module
 
 def build_post_processing(cfg):
@@ -45,7 +56,9 @@ def build_post_processing(cfg):
     
     elif conv_type == "Conv7":
         post_module=torch.nn.Conv2d(256,256,kernel_size=7,padding=3)
-
+        
+    elif conv_type == "NL":
+        post_module=NonLocal2d(256)
     return post_module
 
 
@@ -118,6 +131,217 @@ class GRoIE(ROIPooler):
         roi_feats = self.post_processing(roi_feats)
         return roi_feats
 
+
+
+
+class _NonLocalBlockND(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 reduction=2,
+                 use_scale=True,
+                 conv_cfg=None,
+                 norm_cfg=None,
+                 mode='embedded_gaussian',
+                 **kwargs):
+        super(_NonLocalNd, self).__init__()
+        self.in_channels = in_channels
+        self.reduction = reduction
+        self.use_scale = use_scale
+        self.inter_channels = max(in_channels // reduction, 1)
+        self.mode = mode
+
+        if mode not in [
+                'gaussian', 'embedded_gaussian', 'dot_product', 'concatenation'
+        ]:
+            raise ValueError("Mode should be in 'gaussian', 'concatenation', "
+                             f"'embedded_gaussian' or 'dot_product', but got "
+                             f'{mode} instead.')
+
+        # g, theta, phi are defaulted as `nn.ConvNd`.
+        # Here we use ConvModule for potential usage.
+        self.g = ConvModule(  #Inserire qua la convoluzione che si trova nella riga 27 https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_
+                                                                                        #pytorch_0.3.1/lib/backup/non_local_simple_version.py
+            self.in_channels,
+            self.inter_channels,
+            kernel_size=1,
+            conv_cfg=conv_cfg,
+            act_cfg=None)
+        self.conv_out = ConvModule(    #Inserire qua la convoluzione che si trova nella riga 27 https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_
+                                                                                        #pytorch_0.3.1/lib/backup/non_local_simple_version.py
+            self.inter_channels,
+            self.in_channels,
+            kernel_size=1,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=None)
+
+        if self.mode != 'gaussian':
+            self.theta = ConvModule(  #Inserire qua la convoluzione che si trova nella riga 27 https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_
+                                                                                        #pytorch_0.3.1/lib/backup/non_local_simple_version.py
+                self.in_channels,
+                self.inter_channels,
+                kernel_size=1,
+                conv_cfg=conv_cfg,
+                act_cfg=None)
+            self.phi = ConvModule(    #Inserire qua la convoluzione che si trova nella riga 27 https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_
+                                                                                        #pytorch_0.3.1/lib/backup/non_local_simple_version.py
+                self.in_channels,
+                self.inter_channels,
+                kernel_size=1,
+                conv_cfg=conv_cfg,
+                act_cfg=None)
+
+        if self.mode == 'concatenation':
+            self.concat_project = ConvModule(    #Inserire qua la convoluzione che si trova nella riga 27 https://github.com/AlexHex7/Non-local_pytorch/blob/master/Non-Local_
+                                                                                        #pytorch_0.3.1/lib/backup/non_local_simple_version.py
+                self.inter_channels * 2,
+                1,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                bias=False,
+                act_cfg=dict(type='ReLU'))
+        
+        self.init_weights(**kwargs)
+
+    def init_weights(self, std=0.01, zeros_init=True):
+        if self.mode != 'gaussian':
+            for m in [self.g, self.theta, self.phi]:
+                normal_init(m.conv, std=std)
+        else:
+            normal_init(self.g.conv, std=std)
+        if zeros_init:
+            if self.conv_out.norm_cfg is None:
+                constant_init(self.conv_out.conv, 0)
+            else:
+                constant_init(self.conv_out.norm, 0)
+        else:
+            if self.conv_out.norm_cfg is None:
+                normal_init(self.conv_out.conv, std=std)
+            else:
+                normal_init(self.conv_out.norm, std=std)
+
+    def gaussian(self, theta_x, phi_x):   #NonServeXOra
+
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+
+        pairwise_weight = torch.matmul(theta_x, phi_x)
+        pairwise_weight = pairwise_weight.softmax(dim=-1)
+        return pairwise_weight
+
+    def embedded_gaussian(self, theta_x, phi_x):   #ValoreDiDefault
+
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+
+        pairwise_weight = torch.matmul(theta_x, phi_x)
+        if self.use_scale:
+            # theta_x.shape[-1] is `self.inter_channels`
+            pairwise_weight /= theta_x.shape[-1]**0.5
+        pairwise_weight = pairwise_weight.softmax(dim=-1)
+        return pairwise_weight
+
+    def dot_product(self, theta_x, phi_x):   #NonServeXOra
+        
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+
+        pairwise_weight = torch.matmul(theta_x, phi_x)
+        pairwise_weight /= pairwise_weight.shape[-1]
+        return pairwise_weight
+
+    def concatenation(self, theta_x, phi_x):    #NonServeXOra
+        
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+
+        h = theta_x.size(2)
+        w = phi_x.size(3)
+        theta_x = theta_x.repeat(1, 1, 1, w)
+        phi_x = phi_x.repeat(1, 1, h, 1)
+
+        concat_feature = torch.cat([theta_x, phi_x], dim=1)
+        pairwise_weight = self.concat_project(concat_feature)
+        n, _, h, w = pairwise_weight.size()
+        pairwise_weight = pairwise_weight.view(n, h, w)
+        pairwise_weight /= pairwise_weight.shape[-1]
+
+        return pairwise_weight
+    def forward(self, x):
+        # Assume `reduction = 1`, then `inter_channels = C`
+        # or `inter_channels = C` when `mode="gaussian"`
+
+        # NonLocal2d x: [N, C, H, W]
+
+        n = x.size(0)
+
+        # NonLocal2d g_x: [N, HxW, C]
+
+        g_x = self.g(x).view(n, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        # NonLocal2d theta_x: [N, HxW, C], phi_x: [N, C, HxW]
+
+        if self.mode == 'gaussian':
+            theta_x = x.view(n, self.in_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            if self.sub_sample:
+                phi_x = self.phi(x).view(n, self.in_channels, -1)
+            else:
+                phi_x = x.view(n, self.in_channels, -1)
+        elif self.mode == 'concatenation':
+            theta_x = self.theta(x).view(n, self.inter_channels, -1, 1)
+            phi_x = self.phi(x).view(n, self.inter_channels, 1, -1)
+        else:
+            theta_x = self.theta(x).view(n, self.inter_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            phi_x = self.phi(x).view(n, self.inter_channels, -1)
+
+        pairwise_func = getattr(self, self.mode)
+        # NonLocal2d pairwise_weight: [N, HxW, HxW]
+        pairwise_weight = pairwise_func(theta_x, phi_x)
+
+
+        # NonLocal2d y: [N, HxW, C]
+
+        y = torch.matmul(pairwise_weight, g_x)
+
+        # NonLocal2d y: [N, C, H, W]
+
+        y = y.permute(0, 2, 1).contiguous().reshape(n, self.inter_channels,
+                                                    *x.size()[2:])
+
+        output = x + self.conv_out(y)
+
+        return output
+
+class NonLocal2d(_NonLocalNd):
+    """2D Non-local module.
+    Args:
+        in_channels (int): Same as `NonLocalND`.
+        sub_sample (bool): Whether to apply max pooling after pairwise
+            function (Note that the `sub_sample` is applied on spatial only).
+            Default: False.
+        conv_cfg (None | dict): Same as `NonLocalND`.
+            Default: dict(type='Conv2d').
+    """
+
+    _abbr_ = 'nonlocal_block'
+
+    def __init__(self,
+                 in_channels,
+                 sub_sample=False,
+                 conv_cfg=dict(type='Conv2d'),
+                 **kwargs):
+        super(NonLocal2d, self).__init__(
+            in_channels, conv_cfg=conv_cfg, **kwargs)
+
+        self.sub_sample = sub_sample
+
+        if sub_sample:
+            max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
+            self.g = nn.Sequential(self.g, max_pool_layer)
+            if self.mode != 'gaussian':
+                self.phi = nn.Sequential(self.phi, max_pool_layer)
+            else:
+                self.phi = max_pool_layer
 
 
 @ROI_HEADS_REGISTRY.register()
